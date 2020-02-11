9f274a433d98ce510a344247139a4d8906c758e8
Kirill Novik <kino6052@colorado.edu>
2020-02-11 16:32:07 -0700

    Update Cache

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,7 @@
 self.addEventListener('install', function(event) {
-  console.warn('one')
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.warn('two')
-      return cache.addAll([
-        '/css/bootstrap.css',
-        '/css/main.css',
-        '/js/bootstrap.min.js',
-        '/js/jquery.min.js',
-        '/offline.html',
-      ])
+    caches.open('cache').then(function(cache) {
+      return cache.addAll(['/images'])
     })
   )
 })
